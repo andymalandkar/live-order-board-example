573e3c9e661036b67e24027c46a76706a40ab084
Personal <personalPersonal@tpmbp-tlmd6m.mynet>
2021-07-11 01:12:36 +0100

    Print cancelation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-import { getOrdersSummary, placeOrder } from 'live-order-board-crypto-inc';
+import {
+  cancelOrder,
+  getOrdersSummary,
+  placeOrder,
+} from 'live-order-board-crypto-inc';
 import { orders } from './data/orders';
 
 const executeOrders = async () => {
-  for (var i = 0; i < orders.length; i++) {
-    placeOrder(orders[i]);
+  const orderIds: string[] = [];
+  for (let i = 0; i < orders.length; i++) {
+    const order = orders[i];
+    console.log(`${order.coinType} ${order.orderType} order`);
+
+    orderIds.push(placeOrder(order));
+
+    console.log('Sell Orders summary');
     console.table(getOrdersSummary().sellOrders);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    console.log('Buy Orders summary');
+    console.table(getOrdersSummary().buyOrders);
+    await new Promise((resolve) => setTimeout(resolve, 300));
+  }
+
+  console.log('********Order Cancelation**********');
+  console.table(orderIds);
+  await new Promise((resolve) => setTimeout(resolve, 100));
+
+  for (let i = 0; i < orderIds.length; i++) {
+    cancelOrder(orderIds[i]);
+
+    console.log('Sell Orders summary');
+    console.table(getOrdersSummary().sellOrders);
+
+    console.log('Buy Orders summary');
+    console.table(getOrdersSummary().buyOrders);
+    await new Promise((resolve) => setTimeout(resolve, 300));
   }
 };
 
